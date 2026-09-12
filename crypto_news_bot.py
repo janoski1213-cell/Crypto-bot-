103ae2ecd30c764175d0bf7299ab5020f5c11f4c
janoski1213-cell <janoski1213@gmail.com>
2026-09-12 18:06:39 -0300

    Update crypto_news_bot.py

--- crypto_news_bot.py
+++ crypto_news_bot.py
@@ -1,14 +1,6 @@
 import requests
 import os
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
-try:
-    # Pide noticias directo en español
-    urimport requests
-import os
-
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
@@ -35,9 +27,3 @@ try:
 
 except Exception as e:
     print(f"Error:{e}")
-    requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", json={
-        "chat_id": CHAT_ID,
-        "text": "🤖 Bot activo - Revisando mercado cripto..."
-    }, timeout=15)
-    # Aunque fal
-    })
